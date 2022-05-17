50aa0dd9a1abdfce64625be9702d2a4af6f53171
Dean Blackborough <dean@g3d-development.com>
2022-05-17 17:18:05 +0100

    Initial commit
    - Premake and initial vendor scripts

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+workspace "2DGameEngine"
+    architecture "x64"
+    language "C++"
+    cppdialect "C++latest"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "2DGame"
+    location "2DGame"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin/intermediates/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    
+    includedirs 
+    {
+        "vendor/glm",
+        "vendor/imgui",
+        "vendor/sol",
+        "vendor/sdl/include",
+        "vendor/lua",
+        "vendor/spdlog/include"
+    }
+      
+    libdirs
+    {
+        "vendor/sdl/lib/x64",
+        "vendor/lua/lib",
+    }
+    
+    links 
+    {
+        "SDL2.lib",
+        "SDL2main.lib",
+        "SDL2_image.lib",
+        "SDL2_mixer.lib",
+        "SDL2_ttf.lib",
+    }
+    
+    linkoptions "liblua53.a"
+                 
+    filter "system.windows"
+        cppdialect "C++latest"
+        systemversion "latest"
+        
+    filter { "system:windows" }
+        prebuildcommands { 
+            "xcopy %{prj.location}\\..\\vendor\\lua\\lib\\lua53.dll %{prj.location}\\..\\bin\\%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}\\%{prj.name}\\lua53.dll /d",
+            "xcopy %{prj.location}\\..\\vendor\\sdl\\lib\\x64\\*.dll %{prj.location}\\..\\bin\\%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}\\%{prj.name}\\*.dll /d",
+        }
---
+++ scripts/setup.bat
@@ -0,0 +1,6 @@
+@echo off
+
+pushd ..
+vendor\premake5\premake5.exe vs2022
+popd
+pause
